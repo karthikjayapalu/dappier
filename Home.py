3a62529a4cf6801e09be76ee6cf5a165e7bb8b0b
Karthigeyan R  J <karthikjayapalu@gmail.com>
2024-11-11 12:06:05 +0530

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+# Set up the Streamlit page
+st.title("AI-Powered Interview Agent")
+st.markdown(
+    """
+    AI-Powered Interview Agent is an assignment project performed for Dappier! 
+    ### **👈 Select the insights menu from the sidebar** 
+    
+    ### Insights
+    - to upload the Resume & JD to get AI generated insights!
+    ### Take Interview
+    - then click "Take Interview" Menu to answer the Level 1 interview questions asked by our Agent ! 
+"""
+)
+
